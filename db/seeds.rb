cfd8590696f2ec832e98cb2470d35aa0b48161ff
Morgan Dorian Trias <3975514+MorgueXIIV@users.noreply.github.com>
2021-03-25 20:50:13 +0000

    added a CSV with actors data, wrote a function for db:seed that imports this data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
-# require_relative './actors.csv'
+# require_relative 
 def seed_actors
-  # csv_file_path = './actors.csv'
-  # puts "Seeding users from #{csv_file_path}..."
-  # f = File.new(csv_file_path, 'r')
-  # csv = CSV.new(f)
-  # headers = csv.shift
+	Actor.delete_all
+	csv_file_path = Rails.root.join('db/data/actors.csv')
+	puts "Seeding users from #{csv_file_path}..."
+	f = File.new(csv_file_path, 'r')
+	csv = CSV.new(f)
+	headers = csv.shift
 
-  csv=["HUB",
-  	"Klaasje (Miss Oranje Disco Dancer)",
-"Lena, the Cryptozoologist's wife",
-"Kim Kitsuragi",
-"Soona, the Programmer",
-"Evrart Claire",
-"Garte, the Cafeteria Manager",
-"The Hanged Man",
-"Jean Vicquemare",
-"Judit Minot",
-"Trant Heidelstam",
-"Joyce Messier",
-"Shadow of a Shadow"]
-  
-  csv.each_with_index do |row, idx|
-    a_information = {
-      name: row,
-      id: idx
-    }
-    inv = Actor.create(a_information)
-  end
-  puts 'Seeding users from #{csv_file_path} done.'
+	csv.each do |row|
+		a_information = {
+			name: row[1],
+			id: row[0]
+		}
+		inv = Actor.create(a_information)
+	end
+	puts "Seeding users from #{csv_file_path} done."
 end
 
 seed_actors
