c20a63452a7468c661da9263cdd80f2034887e11
Morgue Dorian Trias <3975514+MorgueXIIV@users.noreply.github.com>
2021-04-22 00:45:05 +0100

    Can now add next and previous steps to conversations, tried switching to postgres SQL and broke shit.

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -3,21 +3,30 @@ class ConversationController < ApplicationController
 	def trace
 		if params[:dialogueid].blank? then
 		else
-			thisDialogue = Dialogue.find(params[:dialogueid])
+			idsList=params[:dialogueid].split("-")
 
-			@builtConvo = []
-			@builtConvo.push(thisDialogue)
+			@builtConvo = idsList.map { |e| Dialogue.find(e) }
+			# for idsList.each do |oneid|
+			# 	thisDialogue = Dialogue.find(oneid)
+			# 	@builtConvo.push(thisDialogue)
+			# end
 
-			@backOptions = thisDialogue.origin.all
+			# thisDialogue = 
+			@backOptions = @builtConvo.first.origin.all
 			while @backOptions.length == 1 do
 				@builtConvo.unshift @backOptions.first
 				@backOptions=@builtConvo.first.origin.all		
 			end
-			@forwOptions = thisDialogue.destination.all
+
+			# thisDialogue = 
+			@forwOptions = @builtConvo.last.destination.all
 			while @forwOptions.length == 1 do
 				@builtConvo.push @forwOptions.first
-				@forwOptions=@builtConvo.last.destination.all		
+				@forwOptions=@builtConvo.last.destination.all
 			end
+
+			idsList=@builtConvo.map { |e| e.id }
+			@idsList=idsList.join("-")
 		end
 	end
 end
