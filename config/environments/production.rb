d6a3b7784c09466c92225ecc8f94fcba3518da52
Morgue Dorian Trias <3975514+MorgueXIIV@users.noreply.github.com>
2021-04-25 13:12:30 +0100

    Updated homepage and fixed a couple of things that caused issues on production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << "fayde.nfshost.com"
+  # config.hosts << "fayde.nfshost.com"
 
-  config.hosts << "fayde.co.uk"
+  # config.hosts << "fayde.co.uk"
 end
