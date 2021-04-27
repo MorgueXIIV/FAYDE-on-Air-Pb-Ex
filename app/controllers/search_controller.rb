9fa8075e8d3dd01bf8a383b14fda3bc1bfaabba3
Morgue Dorian Trias <3975514+MorgueXIIV@users.noreply.github.com>
2021-04-27 18:16:54 +0100

    I made search results look more intuitive quick commit cos travelling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,19 +4,27 @@ class SearchController < ApplicationController
 		query=params[:query]
 		if query.blank? then
 			@results=[]
+			@searchMessage=""
 		else
-			# searchResults = Dialogue.where("dialoguetext LIKE ?", "%#{params[:query]}%").first(500)
+			maxSearchResults=500
 			query=query.split(" ")
-			query=query.reject{|e| e.length<3}
+			commonWords = ["the", "you", "to", "a", "i", "of", "it", "and", "in", "is", "he", "this", "that", "your", "for", "on", "not", "what", "his", "it's"]
+			query=query.reject{ |e| (e.length<2 or (commonWords.index(e) != nil)) }
 			if query.empty? then
 				searchResults = []
+				@searchMessage="Sorry, all the words from your query were filtered out. Please use less common words, and ones which are longer than 1 letter (partial and mid-word matches count, you see, so searchign 'd' would return everything with a 'd' in it, and that's like 53,000 records, you won't want to read all that!)"
 			else
 				query=query.first(10)
-				searchResults = Dialogue.includes(:actor).searchTexter(query).first(500)
+				searchResults = Dialogue.includes(:actor).searchTexter(query).first(maxSearchResults)
+
+				if searchResults.length==maxSearchResults
+					@searchMessage="Your search for '#{ query.join(", ") }' reached the cap of 500 records. \n Perhaps a more specific search is in order?"
+				else
+					@searchMessage="Your search for  '#{ query.join(", ") }' returned #{searchResults.length} dialogue options."
+				end
 			end
 
 			@results=searchResults
-			# @resultStrings = searchResults.map { |result| result.showShort }
 		end
 	end
 end
