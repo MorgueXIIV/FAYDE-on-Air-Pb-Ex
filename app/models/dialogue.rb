9a090f471dd0541acdb4cbe1f456af174550396a
Morgue <3975514+MorgueXIIV@users.noreply.github.com>
2021-05-03 04:06:14 +0100

    links now white on search page, Syb mode given its true name

--- app/models/dialogue.rb
+++ app/models/dialogue.rb
@@ -65,7 +65,9 @@ class Dialogue < ActiveRecord::Base
 
   def showDetails
     lomgpossinfo=[conditionstring,userscript,sequence]
-    alternates.all.each{ |alt| lomgpossinfo.push(alt.showShort)} 
+    if alternates_count > 0
+      alternates.all.each{ |alt| lomgpossinfo.push(alt.showShort)} 
+    end
     lomgpossinfo=lomgpossinfo.reject{|info| info.nil? or info.length<2 }
 
     if difficultypass>0 then
