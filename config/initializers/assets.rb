8697b8f8b0301d74667a030c323b2b9eda281e51
pieartsy <pieartsy@users.noreply.github.com>
2021-05-07 18:58:48 -0400

    changed the font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( dark.css )
 Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
