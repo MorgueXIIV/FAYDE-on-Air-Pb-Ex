0812ac1cf57f86cb9a0bacb60969db1373ce4de3
Morgue <3975514+MorgueXIIV@users.noreply.github.com>
2021-11-03 17:49:56 +0000

    Awww yes, you can now search by variables!!!!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
 		@pageTitle = "Search"
 		query=params[:query]
 		actorLimit=params[:actor]
+		queryType=params[:queryType]
 
 		if (not actorLimit.blank?) then actor=Actor.find_by_name_part(actorLimit) end
 
@@ -19,30 +20,35 @@ class SearchController < ApplicationController
 				query=query.split('"')
 			end
 
-			commonWords = ["the", "you", "to", "a", "i", "of", "it", "and", "in", "is", 
+			commonWords = ["the", "you", "to", "a", "i", "of", "it", "and", "in", "is",
 				"he", "this", "that", "your", "for", "on", "not", "what", "his", "it's"]
 			query=query.reject{ |e| (e.length<2 or (commonWords.index(e.strip) != nil)) }
 
 			if query.empty? then
 				searchResults = []
-				@searchMessages.push """Sorry, all the words from your query were filtered out. 
+				@searchMessages.push """Sorry, all the words from your query were filtered out.
 					Please use less common words, and ones which are longer than 1 letter"""
-				@searchMessages.push """(partial and mid-word matches count, you see, so searching 'd' 
-					would return everything with a 'd' in it, and that's like 53,000 records, 
+				@searchMessages.push """(partial and mid-word matches count, you see, so searching 'd'
+					would return everything with a 'd' in it, and that's like 53,000 records,
 					you won't want to read all that!)"""
 			else
 				query=query.first(10)
 
 				querystringdisplay = query.join(", ")
 				# query=query.map{|e| e.strip }
-				if actor.blank? then
-					searchResults = Dialogue.includes(:actor).searchTexts(query).first(maxSearchResults)
-					@searchMessages.push actorLimit.blank? ? "" : "Sorry, unable to find actor with '#{actorLimit}' in their name. \n"
+				if queryType=="Variables"
+					searchResults = Dialogue.includes(:actor).searchVars(query).first(maxSearchResults)
 				else
-					searchResults = Dialogue.includes(:actor).searchTextsAct(query, actor).first(maxSearchResults)
-					@searchMessages.push "Searching '#{actor.name}' dialogues only. \n"
+					if actor.blank? then
+						searchResults = Dialogue.includes(:actor).searchTexts(query).first(maxSearchResults)
+						@searchMessages.push actorLimit.blank? ? "" : "Sorry, unable to find actor with '#{actorLimit}' in their name. \n"
+					else
+						searchResults = Dialogue.includes(:actor).searchTextsAct(query, actor).first(maxSearchResults)
+						@searchMessages.push "Searching '#{actor.name}' dialogues only. \n"
+					end
 				end
 				# NOTE: by this point "query" is destroyed, because the scope of Ruby passes by reference!
+
 
 
 				if searchResults.length==maxSearchResults
@@ -50,12 +56,15 @@ class SearchController < ApplicationController
 				else
 					@searchMessages.push "Your search for  '#{ querystringdisplay }' returned #{searchResults.length} dialogue options."
 				end
+				if queryType=="Variables" then
+					@searchMessages.push "Searching the variables Checked or Updated for '#{querystringdisplay}'"
+				end
 			end
 
 			@results=searchResults
 		end
 	end
-	
+
 	def conversation_index
 		@pageTitle="Conversations"
 
