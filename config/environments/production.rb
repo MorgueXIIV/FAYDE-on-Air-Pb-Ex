38c06ce7892f944d21b596e2b53106d8da42be39
Morgue <3975514+MorgueXIIV@users.noreply.github.com>
2022-01-15 04:14:59 +0000

    eugh configs updated to get static assets to serve so the robots TXT works etc.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_assets = true
+  config.serve_static_files = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
