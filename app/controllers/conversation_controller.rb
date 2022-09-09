53f5484fc2682132e01580a7504fe788e22e384b
Morgue Dorian Trias <3975514+MorgueXIIV@users.noreply.github.com>
2022-09-09 16:12:42 +0100

    improvements for accessability on screenreaders, part URL shortening

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -21,35 +21,135 @@ class ConversationController < ApplicationController
 				@conversationDescribe=( Conversation.where(id: convoIDs) )
 				convoIDs.each { | convoID | convosLinks += findLinks.call convoID }
 
-			begin
+				pickBLinks = Proc.new { |id| (convosLinks.select { |l| (l[1].to_i == id) }).map { |e| e[0].to_i }}
+				pickFLinks = Proc.new { |id| (convosLinks.select { |l| (l[0].to_i == id) }).map { |e| e[1].to_i }}
+				checkConvoDrift= Proc.new { |newIDs|
+									checkForOutliers = (newIDs.map{ |e| e / 10000 }.uniq) - convoIDs
+										if not checkForOutliers.blank? then
+											convoIDs += checkForOutliers
+											checkForOutliers.each{ |e| findLinks.call e }
+										end}
 
-				# 	# this breaks where two conversations link together so I gotta do this nasty thing to get those other links
-				# 	# make a list of convos we've retrieved
-				# convosWhoseLinksWeHave = convoIDs
-				# 	# flatten array of arrays so it's a list of endpoints and startpoints alike, then divide each by ten thougsand to make it a conversation id, then get only the unique ones, then select the ones that are not already in the links-we-have list.
-				# extraConvosNeeded = ((convosLinks.flatten.map{ |e| (e / 10000) }.uniq) - convosWhoseLinksWeHave)
-				# while not extraConvosNeeded.blank? do
-				# 	extraConvosLinks = []
-				# 	extraConvosNeeded.each do | xlink |
-				# 		min=xlink*10000description
-				# 		max=(xlink+1)*10000
-				# 		extraConvosLinks = extraConvosLinks + DialogueLink.where(origin_id: min..max).or(DialogueLink.where(destination_id: min..max)).pluck(:origin_id, :destination_id)
+			begin # (error catching)
+				idsFullList = Array.new
+				idsCritList = Array.new
+				# forwOptions = pickBLinks.call idsList.first #initialise the forward options so we have the history of the first link
+				forwOptions= Array.new
+
+					thisID=idsList.pop
+					# idsCritList.push thisID
+					begin
+						if forwOptions.length != 1 then
+							idsCritList.push thisID
+						end
+						idsFullList.push thisID
+
+						forwOptions = pickFLinks.call(thisID)
+
+						if not forwOptions.index(idsList[0]).nil? then
+							thisID = idsList.pop
+						elsif forwOptions.length==1
+							thisID = forwOptions[0]
+						else
+							break
+						end
+
+					end	until idsList.empty? and forwOptions.length != 1
+
+
+				#
+				# while not idsList.empty? do
+				# 	if forwOptions.length > 1 then idsCritList.push thisID end
+				# 	forwOptions=pickFLinks.call(thisID)
+				#
+				# 		while forwOptions.length == 1 do
+				# 			idsFullList.push forwOptions[0]
+				# 			if forwOptions[0]==idsList[0]
+				# 				idsList.pop
+				# 			end
+				# 			forwOptions = pickFLinks.call(forwOptions[0])
+				# 			checkConvoDrift.call(forwOptions)
+				# 		end
 				# 	end
-				# 	convosLinks = convosLinks+extraConvosLinks
-				# 	convosWhoseLinksWeHave = convosWhoseLinksWeHave + extraConvosNeeded
-				# 	# re-run this horrible method chain to check if there are more now
-				# 	extraConvosNeeded = ((extraConvosLinks.flatten.map{ |e| (e / 10000) }.uniq) - convosWhoseLinksWeHave)
 				# end
 
-				backOptions = convosLinks.select { |l| (l[1].to_i == idsList[0]) }.map{ |e| e[0].to_i }
+				backOptions = pickBLinks.call(idsFullList[0])
+				while backOptions.length == 1 do
+					idsFullList.unshift backOptions[0]
+					checkConvoDrift.call(backOptions)
+					backOptions = pickBLinks.call idsFullList[0]
+				end
+
+				# forwOptions = convosLinks.select { |l| (l[0].to_i==idsList.last) }.map { |e| e[1].to_i }
+				# while forwOptions.length == 1 do
+				# 	idsList.push forwOptions[0]
+				# 	forwOptions = convosLinks.select { |l| (l[0].to_i==idsList.last) }.map { |e| e[1].to_i }
+				# 	checkForOutliers = (forwOptions.map{ |e| e / 10000 }.uniq) - convoIDs
+				# 	if not checkForOutliers.blank? then
+				# 		convoIDs += checkForOutliers
+				# 		checkForOutliers.each{ |e| findLinks.call e }
+				# 	end
+				# end
+
+				idsRetrieving = (idsFullList+forwOptions)
+				dialoguesUsing = Dialogue.includes(:actor, :alternates, :checks).where(id: idsRetrieving)
+				dialoguesUsing = dialoguesUsing + Dialogue.includes(:actor, :alternates, :checks, :origin).where(id: backOptions)
+
+				@builtConvo = idsFullList.map { |e| dialoguesUsing.find{|f| f.id==e }}
+				#
+				# @forwOptions = forwOptions.map { |e| dialoguesUsing.find{|f| f.id==e}}
+				# @backOptions = backOptions.map { |e| dialoguesUsing.find{|f| f.id==e}} #Dialogue.includes(:actor, :alternates).find_by_id(backOptions)
+				@forwOptions = @builtConvo.last.destination.includes(:actor).all
+				@backOptions = @builtConvo.first.origin.includes(:actor, :origin).all
+
+				@idsList = idsCritList.join("-")
+				@idsLongList = idsFullList.join("-")
+				@idsShortList = idsList.join("-")
+			rescue ActiveRecord::RecordNotFound
+			end
+		end
+	end
+
+	def error
+		@pageTitle = "Error"
+	end
+
+
+	def tracelongurl
+		@pageTitle = "Conversation"
+		if params[:dialogueid].blank? then
+			render :controller => 'conversation', :action => "error"
+		else
+				convosLinksReq= DialogueLink
+				idsList=params[:dialogueid].split("-").map{ |e| e.to_i }
+				@conversationDescribe = []
+				convoIDs= (((idsList.map{ |e| e / 10000 }).uniq))
+				convosLinks=[]
+
+				findLinks = Proc.new { |id|
+									min=id*10000
+									max=(id+1)*10000
+									convosLinksReq = (DialogueLink.where("origin_id between ? and ? or destination_id between ? and ?", min, max, min, max))
+									convosLinks += convosLinksReq.pluck(:origin_id, :destination_id)
+								}
+								pickBLinks = Proc.new { |id| (convosLinks.select { |l| (l[1].to_i == id) }).map { |e| e[0].to_i }}
+								pickFLinks = Proc.new { |id| (convosLinks.select { |l| (l[0].to_i == id) }).map { |e| e[1].to_i }}
+								checkConvoDrift= Proc.new { |newIDs|
+													checkForOutliers = (newIDs.map{ |e| e / 10000 }.uniq) - convoIDs
+														if not checkForOutliers.blank? then
+															convoIDs += checkForOutliers
+															checkForOutliers.each{ |e| findLinks.call e }
+														end}
+				@conversationDescribe=( Conversation.where(id: convoIDs) )
+				convoIDs.each { | convoID | convosLinks += findLinks.call convoID }
+
+			begin # (error catching)
+
+				backOptions = pickBLinks.call(idsList[0])
 				while backOptions.length == 1 do
 					idsList.unshift backOptions[0]
-					backOptions = convosLinks.select{ |l| (l[1].to_i==idsList[0]) }.map { |e| e[0].to_i }
-					checkForOutliers = (backOptions.map{ |e| e / 10000 }.uniq) - convoIDs
-					if not checkForOutliers.blank? then
-						convoIDs += checkForOutliers
-						checkForOutliers.each{ |e| findLinks.call e }
-					end
+					backOptions = pickBLinks.call idsList[0]
+					checkConvoDrift.call(backOptions)
 				end
 
 				forwOptions = convosLinks.select { |l| (l[0].to_i==idsList.last) }.map { |e| e[1].to_i }
@@ -75,6 +175,8 @@ class ConversationController < ApplicationController
 				@backOptions = @builtConvo.first.origin.includes(:actor, :origin).all
 
 				@idsList= idsList.join("-")
+				@idsLongList = "-"
+				@idsShortList = "-"
 			rescue ActiveRecord::RecordNotFound
 			end
 		end
